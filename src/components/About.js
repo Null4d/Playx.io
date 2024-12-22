643c9085acb9a59aa8930177689f58b44d7dc9d8
Null <batubutu2077@gmail.com>
2024-12-22 18:12:05 +0400

    test push

--- src/components/About.js
+++ src/components/About.js
@@ -39,6 +39,7 @@ function About() {
                                 <button className="">
 
                                 </button>
+                                
                             </li>
                         </ul>
                     </div>
