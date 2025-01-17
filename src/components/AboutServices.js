65fb2541bb366887795a708ebe774fc4463bf1cf
Null <batubutu2077@gmail.com>
2025-01-17 18:51:12 +0400

    fis issues

--- src/components/AboutServices.js
+++ src/components/AboutServices.js
@@ -15,9 +15,9 @@ function AboutServices() {
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setIndex((prevIndex) => (prevIndex + 1) % images.length); // Resimleri sırayla geç
-        }, 3000); // 3 saniyede bir
-        return () => clearInterval(interval); // Temizleme işlemi
+            setIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 4000);
+        return () => clearInterval(interval);
     }, [images.length]);
 
     return (
@@ -25,73 +25,75 @@ function AboutServices() {
             <section className="pt-32 pb-32 bg-[#040608]">
                 <div className="container mx-auto">
                     <div className="relative flex flex-wrap items-end mx-4 align-items-xl-start">
-
                         <div className="relative w-6/12 px-4 basis-6/12">
-                            <div className="section__title text-left mb-[65px] relative after:content-[''] after:block after:w-[65px] after:h-[5px] after:mt-5 after:mb-0 after:mx-0">
+                            <div className="section__title text-left mb-[65px] relative after:content-[''] after:block after:w-20 after:h-1.5 after:bg-green after:mt-5 after:[clip-path:polygon(0%_0%,_100%_0%,_90%_100%,_10%_100%)]">
                                 <span className="sub-title tg__animate-text block uppercase text-[14px] tracking-[2px] font-semibold text-green leading-none mt-0 mb-[7px] mx-0 ready stop">
                                     powerful services
                                 </span>
-                                <h3 className="title text-[45px] text-white font-extrabold uppercase tracking-[1px] m-0">
+                                <h3 className="title text-5xl text-white font-extrabold uppercase tracking-[1px] m-0 ">
                                     Our Powerful Services Done on time
                                 </h3>
                             </div>
                             <div className="grid grid-cols-[repeat(2,1fr)] xsm:grid-cols-[repeat(1,1fr)] gap-[60px_50px] text-left">
                                 <div>
-                                    <div className="text-[40px] block leading-none text-green mt-0 mb-[30px] mx-0">
+                                    <div className="block mx-0 mt-0 mb-8 text-4xl leading-none text-green">
                                         <i className="flaticon-diamond"></i>
                                     </div>
                                     <div>
-                                        <h4 className="title text-2xl text-white font-bold capitalize tracking-[1.2px] mt-0 mb-[18px] mx-0">
+                                        <h4 className="mx-0 mt-0 mb-5 text-2xl font-bold tracking-wider text-white capitalize title">
                                             <a className="hover:text-green" href="services-details.html">
                                                 Year Experience
                                             </a>
                                         </h4>
-                                        <p className="text-[15px] text-grey font-bold m-0">
+                                        <p className="m-0 text-base font-bold text-grey">
                                             Lorem ipsum dolor sitamet const adipiscng Duis eletum sollicitudin is yaugue euismods
                                         </p>
                                     </div>
                                 </div>
+
                                 <div>
-                                    <div className="text-[40px] block leading-none text-green mt-0 mb-[30px] mx-0">
+                                    <div className="block mx-0 mt-0 mb-8 text-4xl leading-none text-green">
                                         <i className="flaticon-user-profile"></i>
                                     </div>
                                     <div>
-                                        <h4 className="title text-2xl text-white font-bold capitalize tracking-[1.2px] mt-0 mb-[18px] mx-0">
+                                        <h4 className="mx-0 mt-0 mb-5 text-2xl font-bold tracking-wider text-white capitalize title">
                                             <a className="hover:text-green" href="services-details.html">
                                                 Expert Teams
                                             </a>
                                         </h4>
-                                        <p className="text-[15px] text-grey font-bold m-0">
+                                        <p className="m-0 text-base font-bold text-grey">
                                             Lorem ipsum dolor sitamet const adipiscng Duis eletum sollicitudin is yaugue euismods
                                         </p>
                                     </div>
                                 </div>
+
                                 <div>
-                                    <div className="text-[40px] block leading-none text-green mt-0 mb-[30px] mx-0">
+                                    <div className="block mx-0 mt-0 mb-8 text-4xl leading-none text-green">
                                         <i className="flaticon-ethereum"></i>
                                     </div>
                                     <div>
-                                        <h4 className="title text-2xl text-white font-bold capitalize tracking-[1.2px] mt-0 mb-[18px] mx-0">
+                                        <h4 className="mx-0 mt-0 mb-5 text-2xl font-bold tracking-wider text-white capitalize title">
                                             <a className="hover:text-green" href="services-details.html">
                                                 Best NFT Game
                                             </a>
                                         </h4>
-                                        <p className="text-[15px] text-grey font-bold m-0">
+                                        <p className="m-0 text-base font-bold text-grey">
                                             Lorem ipsum dolor sitamet const adipiscng Duis eletum sollicitudin is yaugue euismods
                                         </p>
                                     </div>
                                 </div>
+                                
                                 <div>
-                                    <div className="text-[40px] block leading-none text-green mt-0 mb-[30px] mx-0">
+                                    <div className="block mx-0 mt-0 mb-8 text-4xl leading-none text-green">
                                         <i className="flaticon-settings-1"></i>
                                     </div>
                                     <div>
-                                        <h4 className="title text-2xl text-white font-bold capitalize tracking-[1.2px] mt-0 mb-[18px] mx-0">
+                                        <h4 className="mx-0 mt-0 mb-5 text-2xl font-bold tracking-wider text-white capitalize title">
                                             <a className="hover:text-green" href="services-details.html">
                                                 Worldwide Client
                                             </a>
                                         </h4>
-                                        <p className="text-[15px] text-grey font-bold m-0">
+                                        <p className="m-0 text-base font-bold text-grey">
                                             Lorem ipsum dolor sitamet const adipiscng Duis eletum sollicitudin is yaugue euismods
                                         </p>
                                     </div>
@@ -113,14 +115,15 @@ function AboutServices() {
                             </div>
 
                         </div>
-                        {/* Right Side - Image Slider */}
+                        {/* Right Side */}
 
                         <div className='absolute top-0 bottom-0 left-0 right-0'>
                             <a href="#b"
-                                className="absolute duration-500 right-0 rounded-50% bottom-0 w-[153px] h-[153px] text-[98px] flex items-center justify-center rotate-45 bg-[#faa706] text-black rounded-full hover:rotate-0 hover:text-black">
+                                className="absolute duration-500 right-0 rounded-50% bottom-0 w-40 h-40 text-[98px] flex items-center justify-center rotate-45 bg-yellow-500 text-black rounded-full hover:rotate-0 hover:text-black">
                                 <i className="flaticon-next"></i>
                             </a>
                         </div>
+                        {/* Right Side - Arrow */}
                     </div>
                 </div>
             </section>
